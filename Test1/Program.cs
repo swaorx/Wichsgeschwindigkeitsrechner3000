1fe65653887acf4499f38917f6a7443d3540acab
swaorx <64856952+swaorx@users.noreply.github.com>
2023-12-07 00:57:20 +0100

    User-Input hinzugefügt. wirdGewichst bool entfernt.

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Test1
 {
@@ -13,43 +15,47 @@ namespace Test1
             // Mathematisch belegte Funktionen für den
             // Wichsgeschwindigkeitsrechner3000 auf GeoGebra: https://www.geogebra.org/calculator/vxukknrf
 
+            // Stargeschwindigkeit beim wichsen v0
+            double wichsGeschAusg;
             // Intensität (Multiplikator des wichsens)
-            double wichsIntensität = 1.1;
+            double wichsIntensität;
             // Wichsbeschleunigung in Masturbationen/Sekunde^2
-            double wichsBeschleunigung = 1.2;
+            double wichsBeschleunigung;
             // Zeit für die gewichst werden soll
-            double wichsZeit = 15;
-            // Ob gewichst werden soll
-            bool wirdGewichst = true;
+            double wichsZeit;
 
-            Console.WriteLine("Wichsgeschwindigkeitsrechner3000 D:");
-            
-            // Beendet das Programm, wenn nicht gewichst wird.
-            if (wirdGewichst == true)
-            {
-                Console.WriteLine("ACHTUNG, ES WIRD GEWICHST!!!");
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Es wird leider nicht gewichst... :c");
-                Console.ReadLine();
-                Environment.Exit(0);
-            }
 
-            for (int i = 1; i <= wichsZeit; i++)
+            Console.WriteLine("Wichsgeschwindigkeitsrechner3000");
+
+            Console.WriteLine("v(t) = a * t * i + v0");
+            Console.WriteLine();
+
+            Console.Write("Anfangsgeschwindigkeit in Masturbationen/Sekunde: ");
+            wichsGeschAusg = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("i) Wichs-Intensität: ");
+            wichsIntensität = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("a) Wichs-Beschleunigung: ");
+            wichsBeschleunigung = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Dauer in Sekunden, für die gewichst wird: ");
+            wichsZeit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i <= wichsZeit; i++)
             {
                 Console.WriteLine("DIE MOMENTANE WICHSGESCHWINDIGKEIT v BETRÄGT:");
 
-                // Geschwindigkeit (v) nach Geschwindigkeits-Zeit-Gesetz "v = a * t * i"
-                // Geschwindigkeit = Beschleunigung * Zeit. Wird mit Intensität multipliziert.
-                Console.WriteLine(wichsBeschleunigung * i * wichsIntensität + " M/s (MASTURBATIONEN/SEKUNDE)...");
+                // Geschwindigkeit (v) nach Geschwindigkeits-Zeit-Gesetz "v = a * t * i + v0"
+                // Geschwindigkeit = Beschleunigung * Zeit + Ausgangsgeschwindigkeit v0. Wird mit Intensität multipliziert.
+                Console.WriteLine(wichsBeschleunigung * i * wichsIntensität + wichsGeschAusg + " M/s (MASTURBATIONEN/SEKUNDE)...");
 
                 Console.WriteLine("UND DIE ABMASTURBIERTE COCKSTRECKE BETRÄGT:");
 
-                // Bewegungsgesetz der gleichmäßig beschleunigten Bewegung "s = (v^2)/(2*a)"
+                // Bewegungsgesetz der gleichmäßig beschleunigten Bewegung "s = (v(t)^2)/(2*a)"
                 // wobei v durch "wichsBeschleunigung * i * wichsIntensität" (Beschleunigung * Zeit * Intensität) dargestellt wird.
-                Console.WriteLine(((wichsBeschleunigung * i * wichsIntensität)*(wichsBeschleunigung * i * wichsIntensität)) / (2 * wichsBeschleunigung) + " COCKS...");
+                Console.WriteLine(Math.Pow((wichsBeschleunigung * i * wichsIntensität + wichsGeschAusg), 2) / (2 * wichsBeschleunigung) + " COCKS...");
                 
 
                 // wenn laufende wichs-Zeit 1s beträgt, wird "SEKUNDE" ausgegeben. Sonst "SEKUNDEN".
