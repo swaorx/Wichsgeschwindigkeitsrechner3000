423964819de1117e094936554146cfa91e4f66df
swaorx <64856952+swaorx@users.noreply.github.com>
2023-12-06 18:48:07 +0100

    Projektdateien hinzufügen.

---
+++ Test1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mathematisch belegte Funktionen für den
+            // Wichsgeschwindigkeitsrechner3000 auf GeoGebra: https://www.geogebra.org/calculator/vxukknrf
+
+            // Intensität (Multiplikator des wichsens)
+            double wichsIntensität = 1.1;
+            // Wichsbeschleunigung in Masturbationen/Sekunde^2
+            double wichsBeschleunigung = 1.2;
+            // Zeit für die gewichst werden soll
+            double wichsZeit = 15;
+            // Ob gewichst werden soll
+            bool wirdGewichst = true;
+
+            Console.WriteLine("Wichsgeschwindigkeitsrechner3000 D:");
+            
+            // Beendet das Programm, wenn nicht gewichst wird.
+            if (wirdGewichst == true)
+            {
+                Console.WriteLine("ACHTUNG, ES WIRD GEWICHST!!!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Es wird leider nicht gewichst... :c");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            for (int i = 1; i <= wichsZeit; i++)
+            {
+                Console.WriteLine("DIE MOMENTANE WICHSGESCHWINDIGKEIT v BETRÄGT:");
+
+                // Geschwindigkeit (v) nach Geschwindigkeits-Zeit-Gesetz "v = a * t * i"
+                // Geschwindigkeit = Beschleunigung * Zeit. Wird mit Intensität multipliziert.
+                Console.WriteLine(wichsBeschleunigung * i * wichsIntensität + " M/s (MASTURBATIONEN/SEKUNDE)...");
+
+                Console.WriteLine("UND DIE ABMASTURBIERTE COCKSTRECKE BETRÄGT:");
+
+                // Bewegungsgesetz der gleichmäßig beschleunigten Bewegung "s = (v^2)/(2*a)"
+                // wobei v durch "wichsBeschleunigung * i * wichsIntensität" (Beschleunigung * Zeit * Intensität) dargestellt wird.
+                Console.WriteLine(((wichsBeschleunigung * i * wichsIntensität)*(wichsBeschleunigung * i * wichsIntensität)) / (2 * wichsBeschleunigung) + " COCKS...");
+                
+
+                // wenn laufende wichs-Zeit 1s beträgt, wird "SEKUNDE" ausgegeben. Sonst "SEKUNDEN".
+                if (i != 1)
+                {
+                    Console.WriteLine("...NACH NUR " + i + " SEKUNDEN! :O");
+                }
+                else
+                {
+                    Console.WriteLine("...NACH NUR " + i + " SEKUNDE! :O");
+
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Test1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("344e6a06-3402-4b1f-94f2-f440ae0e0641")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
